35627819773f46275d5d9ddd77dff01e3808ac63
jeffersongoncalves <gerson.simao.92@gmail.com>
2026-06-30 19:11:00 +0000

    P3 backport to Filament v3 (1.x)

--- src/Resources/Promotion/Tables/PromotionTable.php
+++ src/Resources/Promotion/Tables/PromotionTable.php
@@ -2,7 +2,7 @@
 
 namespace JeffersonGoncalves\FilamentCommerce\Promotion\Resources\Promotion\Tables;
 
-use Filament\Actions;
+use Filament\Tables\Actions;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 
@@ -16,11 +16,11 @@ class PromotionTable
                     ->searchable()
                     ->sortable(),
             ])
-            ->recordActions([
+            ->actions([
                 Actions\EditAction::make(),
                 Actions\DeleteAction::make(),
             ])
-            ->toolbarActions([
+            ->bulkActions([
                 Actions\BulkActionGroup::make([
                     Actions\DeleteBulkAction::make(),
                 ]),
